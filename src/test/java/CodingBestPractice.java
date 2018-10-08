a38c7affb81649410717608145b4c94a2e7eb32d
sasubilli <ramasu07@gmail.com>
2018-10-08 19:14:50 -0400

    first commit

---
+++ src/test/java/CodingBestPractice.java
@@ -0,0 +1,8 @@
+public class CodingBestPractice{// class begins
+
+public void codingBestPractice(){//codingbestpractice method begins
+
+    //business logic starts
+
+}//codingbestpractice ends
+}//class ends
